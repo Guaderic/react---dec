cfd10e6a8ad8ae52fb61268ef782af7fb4763a7e
Guaderic <VladymyrGolovan@gmail.com>
2022-04-15 20:45:29 +0300

    add lesson 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+//import {Button} from "./components";
+import {Routes,Route, Navigate} from "react-router-dom";
+import {MainLayOut} from "./layouts";
+import {AboutPage, HomePage, NotFoundPage, PostPage, SinglePostPage, UserPage} from "./pages";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path={'/'} element={<MainLayOut/>}>
+            <Route index element={<Navigate to={'home'}/>} />
+            <Route path={'home'} element={<HomePage/>}/>
+            <Route path={'users'} element={<UserPage/>}/>
+            <Route path={'posts'} element={<PostPage/>}>
+                <Route path={':id'} element={<SinglePostPage/>}/>
+            </Route>
+            <Route path={'about'} element={<AboutPage/>}/>
+            <Route path={'*'} element={<NotFoundPage/>}/>
+
+        </Route>
+
+        </Routes>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export * from './HomePage/HomePage';
+export * from './NotFoundPage/NotFoundPage';
+export * from './AboutPage/AboutPage';
+export * from './PostsPage/PostPage';
+export * from './UsersPage/UserPage';
+export * from './singlePostPage/SinglePostPage'
