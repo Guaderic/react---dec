bb823b3b5aa66a382f3f23e0589cb3d8c42854b7
Guaderic <VladymyrGolovan@gmail.com>
2022-05-10 15:14:56 +0300

    add test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
+import {Routes} from "react-router-dom";
 
 
 function App() {
-  return (
-      <Routes>
-
-
+    return (
+        <Routes>
+            <App/>
         </Routes>
-  );
+    );
 }
 
 export default App;
